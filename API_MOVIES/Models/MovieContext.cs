e93818e3128747b6a75d764303b7ca99ec398da8
Patrizio Milione <patriziomilione@gmail.com>
2023-05-20 13:28:59 +0200

    first commit

---
+++ API_MOVIES/Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_MOVIES.Models
+{
+    public class MovieContext : DbContext
+    {
+
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
---
+++ API_MOVIES/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿namespace API_MOVIES.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Genre { get; set; }  
+
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
